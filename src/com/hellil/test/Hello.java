8cc7b63156f2fea19a5ec119de8062fb0ed7e752
Gaydash <gaydash.olga@gmail.com>
2016-08-22 06:32:34 -0500

    first commit

---
+++ src/com/hellil/test/Hello.java
@@ -0,0 +1,11 @@
+package com.hellil.test;
+
+/**
+ * Created by Best on 22.08.2016.
+ */
+public class Hello {
+    public static void main(String argv [] )    {
+        System.out.println("Hello World!");
+
+    }
+}
